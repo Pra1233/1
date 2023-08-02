b76bb9f3b96305a9596e54b3c349346cc68594da
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-08-02 15:10:38 +0530

    Done

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express=require('express');
+const path= require('path');
 const bodyParser=require('body-parser');
 const app=express();
 
-const admin=require('./routes/admin');
-const shop=require('./routes/shop');
+const message=require('./routes/message');
+const user=require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended:false})); 
+app.use(express.static(path.join(__dirname,'public'))); //css
 
-app.use('/admin',admin);
-app.use('/shop',shop);
+
+app.use(message);
+app.use('/',user);
+
 
 app.use((req,res,next)=>{
-res.status(404).send('<h2>Page Not Found</h2>');
+res.status(404).sendFile(path.join(__dirname,'views','pagenotfound.html'));
 })
 
 app.listen(3000);
